7a96700b631162e1341b35d205bdb89da24d5e30
Kevin Gadd <kevin.gadd@gmail.com>
2010-11-15 03:10:50 -0800

    Wait for EVE's main window to appear, and then wait a few more seconds before injecting our payload to prevent crashes

--- Profile_EVE/EVE.cs
+++ Profile_EVE/EVE.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using ShootBlues;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Squared.Task;
 
 namespace ShootBlues.Profile {
     public class EVE : SimpleExecutableProfile {
@@ -11,6 +14,32 @@ namespace ShootBlues.Profile {
             get {
                 return "EVE Online";
             }
+        }
+
+        private IEnumerator<object> BaseOnNewProcess (Process process) {
+            return base.OnNewProcess(process);
+        }
+
+        protected override IEnumerator<object> OnNewProcess (Process process) {
+            Console.WriteLine("Waiting for EVE to start...");
+
+            IntPtr hWnd = IntPtr.Zero;
+            while ((hWnd == IntPtr.Zero) && (!process.HasExited)) {
+                yield return new Sleep(1);
+
+                hWnd = Win32.FindWindow("triuiScreen", "EVE");
+            }
+
+            if (hWnd == IntPtr.Zero) {
+                Console.WriteLine("EVE exited without starting (crashed?)");
+                yield break;
+            }
+
+            Console.WriteLine("EVE started. Giving it a few seconds to clean house...");
+
+            yield return new Sleep(5);
+
+            yield return BaseOnNewProcess(process);
         }
     }
 }
