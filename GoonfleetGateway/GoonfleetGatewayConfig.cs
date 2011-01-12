98173a1cad6fc596a71a1d85fe03d47c8f823310
Kevin Gadd <kevin.gadd@gmail.com>
2011-01-11 17:44:47 -0800

    Added an implementation of the goonfleet chat bot gateway protocol

---
+++ GoonfleetGateway/GoonfleetGatewayConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Squared.Task;
+using Squared.Task.Data.Mapper;
+using Squared.Util.Bind;
+
+namespace ShootBlues.Script {
+    public partial class GoonfleetGatewayConfig : GoonfleetGatewayConfigPanel {
+        public GoonfleetGatewayConfig (GoonfleetGateway script)
+            : base(script) {
+            InitializeComponent();
+
+            Prefs = new IBoundMember[] {
+                BoundMember.New(() => URI.Text),
+                BoundMember.New(() => Username.Text),
+                BoundMember.New(() => Key.Text),
+                BoundMember.New(() => Target.Text)
+            };
+        }
+    }
+
+    public class GoonfleetGatewayConfigPanel : SimpleConfigPanel<GoonfleetGateway> {
+        public GoonfleetGatewayConfigPanel ()
+            : base(null) {
+        }
+
+        public GoonfleetGatewayConfigPanel (GoonfleetGateway script)
+            : base(script) {
+        }
+    }
+}
---
+++ GoonfleetGateway/GoonfleetGatewayConfig.designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ShootBlues.Script {
+    partial class GoonfleetGatewayConfig {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.URI = new System.Windows.Forms.TextBox();
+            this.Target = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Key = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Username:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 16);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Target:";
+            // 
+            // URI
+            // 
+            this.URI.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.URI.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.URI.Location = new System.Drawing.Point(87, 3);
+            this.URI.Name = "URI";
+            this.URI.Size = new System.Drawing.Size(258, 23);
+            this.URI.TabIndex = 9;
+            // 
+            // Target
+            // 
+            this.Target.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.Target.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Target.Location = new System.Drawing.Point(87, 90);
+            this.Target.Name = "Target";
+            this.Target.Size = new System.Drawing.Size(258, 23);
+            this.Target.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "URI:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Key:";
+            // 
+            // Username
+            // 
+            this.Username.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.Username.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(87, 32);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(258, 23);
+            this.Username.TabIndex = 11;
+            // 
+            // Key
+            // 
+            this.Key.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.Key.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Key.Location = new System.Drawing.Point(87, 61);
+            this.Key.Name = "Key";
+            this.Key.PasswordChar = '●';
+            this.Key.Size = new System.Drawing.Size(258, 23);
+            this.Key.TabIndex = 13;
+            // 
+            // GoonfleetGatewayConfig
+            // 
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.URI);
+            this.Controls.Add(this.Target);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.Key);
+            this.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.MinimumSize = new System.Drawing.Size(300, 120);
+            this.Name = "GoonfleetGatewayConfig";
+            this.Size = new System.Drawing.Size(348, 120);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox URI;
+        private System.Windows.Forms.TextBox Target;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Key;
+
+
+
+    }
+}
